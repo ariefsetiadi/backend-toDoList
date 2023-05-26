1df7383c442bedf1ddd215455361c620505fc9e6
Arief Setiadi <arief.setiadi19@gmail.com>
2023-05-26 20:54:02 +0700

    add function for get user by id, update user, and reset password for user

--- controllers/User.js
+++ controllers/User.js
@@ -16,14 +16,49 @@ export const getAllUser = async (req, res) => {
     });
     res.json(users);
   } catch (error) {
-    console.log(error);
+    res.status(400).json({ msg: "Something wrong" });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      attributes: [
+        "id",
+        "fullname",
+        "email",
+        "status",
+        "change_password",
+        "createdAt",
+        "updatedAt",
+      ],
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!user) return res.status(404).json({ msg: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ msg: "Something wrong" });
   }
 };
 
 export const saveUser = async (req, res) => {
   const { fullname, email } = req.body;
 
-  //   Password default
+  // Check duplicate email
+  const checkEmail = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (checkEmail) {
+    return res.status(409).json({ msg: "Email already used" });
+  }
+
+  // Password default
   const defPassword = "Pass12345";
 
   const salt = await bcrypt.genSalt();
@@ -39,5 +74,82 @@ export const saveUser = async (req, res) => {
     res.json({ msg: "Save user is success" });
   } catch (error) {
     res.json({ msg: "Save user is failed" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+
+  // Check duplicate email
+  const checkEmail = await User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+
+  if (checkEmail && checkEmail.id != req.params.id) {
+    return res.status(409).json({ msg: "Email already used" });
+  }
+
+  try {
+    await User.update(
+      {
+        fullname: req.body.fullname,
+        email: req.body.email,
+        status: req.body.status,
+      },
+      {
+        where: {
+          id: user.id,
+        },
+      }
+    );
+
+    res.json({ msg: "Update user is success" });
+  } catch (error) {
+    res.json({ msg: "Update user is failed" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+
+  // Password default
+  const defPassword = "Pass12345";
+
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(defPassword, salt);
+
+  try {
+    await User.update(
+      {
+        password: hashPassword,
+      },
+      {
+        where: {
+          id: user.id,
+        },
+      }
+    );
+
+    res.json({ msg: "Reset password user is success" });
+  } catch (error) {
+    res.json({ msg: "Reset password user is failed" });
   }
 };
--- routes/routeUser.js
+++ routes/routeUser.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { getAllUser, saveUser } from "../controllers/User.js";
+import {
+  getAllUser,
+  getUserById,
+  saveUser,
+  updateUser,
+  resetPassword,
+} from "../controllers/User.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/user", verifyToken, getAllUser);
+router.get("/user/:id", verifyToken, getUserById);
 router.post("/user", verifyToken, saveUser);
+router.patch("/user/:id", verifyToken, updateUser);
+router.patch("/user/reset-password/:id", verifyToken, resetPassword);
 
 export default router;
